48f8158636c2141c7061b5acd263a39ec030991d
Seraphim-is-grumpy <ryozo.matsuno@gmail.com>
2022-03-09 11:33:08 +0900

    auth1-1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+export default function App() {
+  return (
+    <Authenticator>
+    {({ signOut, user }) => (
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hi {user.username}! We now have Auth!</h1>
+      </header>
+      <button onClick={signOut} class="amplify-button" data-variation="primary" type="submit">Sign out</button>
+    </div>
+    )}
+    </Authenticator>
+  );
+}
